0159d9ba93ad65d36eda148217207f992352397a
Se7enSecondz <chris.hao@hotmail.com>
2019-09-05 21:37:42 -0400

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+docker build -t chrishao/multi-client:latest -t chrishao/multi-client:$SHA -f ./client/Dockerfile ./client
+
+docker build -t chrishao/multi-server:latest -t chrishao/multi-server:$SHA -f ./server/Dockerfile ./server
+
+docker build -t chrishao/nulti-worker:latest -t chrishao/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push chrishao/multi-client:latest
+docker push chrishao/multi-client:$SHA
+
+docker push chrishao/multi-server:latest
+docker push chrishao/multi-server:$SHA
+
+docker push chrishao/multi-worker:latest
+docker push chrishao/multi-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/server-deployment client=chrishao/multi-server:$SHA
+
+kubectl set image deployments/client-deployment client=chrishao/multi-client:$SHA
+
+kubectl set image deployments/worker-deployment client=chrishao/multi-worker:$SHA
